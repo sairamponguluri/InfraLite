0f978ad411cee58349e0286204fb1462cdb9e74d
ram <pongulurisairam@gmail.com>
2025-05-05 13:51:47 -0500

    Fix: Remove SSH from Terraform to prevent Github Actions hang

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,28 +5,15 @@ provider "aws" {
 }
 
 resource "aws_instance" "infralite_server" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1)
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2
   instance_type = "t2.micro"
 
   tags = {
     Name = "InfraLiteServer"
   }
+}
 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum update -y",
-      "sudo yum install docker -y",
-      "sudo service docker start",
-      "sudo usermod -a -G docker ec2-user",
-      "docker run -d -p 80:8000 ghcr.io/sairamponguluri/infralite-backend"
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      private_key = file("~/.ssh/id_rsa")
-      host        = self.public_ip
-    }
-  }
+output "public_ip" {
+  value = aws_instance.infralite_server.public_ip
 }
 
